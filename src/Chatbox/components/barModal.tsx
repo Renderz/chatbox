ff267525c1c4696ffcf736146428ae4c9e21278f
Renderz <464816158@qq.com>
2022-05-31 15:36:29 +0800

    增加header,sider样式

--- src/Chatbox/components/barModal.tsx
+++ src/Chatbox/components/barModal.tsx
@@ -6,7 +6,7 @@ import { BarUtils } from '../hooks';
 const { Text } = Typography;
 
 const siderData = Array.from(Array(100)).map((r, i) => ({
-  id: i,
+  id: String(i),
   name: `用户${i}`,
   date: '17:53',
   preview: '阿斯顿发送到发送到发送到发送到发送到发送到发送到发送到发撒打发士大夫撒地方',
@@ -28,17 +28,25 @@ const siderData = Array.from(Array(100)).map((r, i) => ({
 // ];
 
 const BarModal: React.FC = () => {
-  const { modalProps } = BarUtils.useContainer();
+  const { modalProps, handleClickItem, activeKey } = BarUtils.useContainer();
 
   return (
-    <Modal {...modalProps} width="1200px">
+    <Modal {...modalProps} width="1200px" bodyStyle={{ padding: 0 }}>
       <Row>
-        <Col flex="280px" style={{ borderRight: '1px solid black' }}>
+        <Col flex="280px" style={{ borderRight: '1px solid rgb(0 0 0 / 5%)' }}>
           <Scrollbars style={{ height: '600px' }} autoHide>
             <List
               dataSource={siderData}
               renderItem={(item) => (
-                <List.Item extra={item.date}>
+                <List.Item
+                  extra={item.date}
+                  style={{
+                    paddingRight: '16px',
+                    paddingLeft: '16px',
+                    backgroundColor: item.id === activeKey ? '#f0f0f0' : '',
+                  }}
+                  onClick={() => handleClickItem(item.id)}
+                >
                   <List.Item.Meta
                     avatar={<Avatar>{item.name}</Avatar>}
                     title={item.name}
@@ -49,7 +57,21 @@ const BarModal: React.FC = () => {
             />
           </Scrollbars>
         </Col>
-        <Col flex="1">right</Col>
+        <Col flex="1" style={{ display: 'flex', flexDirection: 'column' }}>
+          <div
+            style={{
+              flex: '0 0 50px',
+              borderBottom: '1px solid rgb(0 0 0 / 5%)',
+              paddingLeft: '16px',
+              lineHeight: '50px',
+              fontSize: '16px',
+              fontWeight: '400',
+            }}
+          >
+            {siderData.find((r) => r.id === activeKey)?.name}
+          </div>
+          <div style={{ flex: '1' }}>456</div>
+        </Col>
       </Row>
     </Modal>
   );
